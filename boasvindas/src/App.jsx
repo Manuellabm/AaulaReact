a8c65cc4f902507e84855a5f5587be45aa7b91c0
Manuellabm <martinsmanuella962@gmail.com>
2025-04-08 14:57:21 -0300

    08/04/2024
    matéria  nova

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,22 @@
+import BoasVindas from "./Exemplo1"
+
+function App() {
+ 
+
+  return (
+   <div>
+    <h1>Boas vindas ao React com JavaScript</h1>
+    <p>
+      Este é o arquivo App.jsx que séra o nosso ponto de partida para o desenvolvimento de aplicação React ultilizando JavaScript
+    </p>
+
+    <BoasVindas nome="Manuella" />
+    <BoasVindas nome="Milena" />
+    <BoasVindas nome="Marina" />
+    <BoasVindas nome="Pietra" />
+    <BoasVindas nome="Isabelly" />
+   </div>
+  )
+}
+
+export default App
