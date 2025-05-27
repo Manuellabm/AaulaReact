1d01c8df3db03b82365480e02c869bdbb1425454
Manuellabm <martinsmanuella962@gmail.com>
2025-05-27 14:56:37 -0300

    27/05
    27/05

---
+++ exercicio2/src/App.jsx
@@ -0,0 +1,14 @@
+import Exercicio2 from "./Exercicio2";
+export default function App()
+{
+  return(
+    <div>
+      <h1>Chamado para Exercicio2</h1>
+      <Exercicio2 temperatura = {10} />
+      <Exercicio2 temperatura = {30} />
+      <Exercicio2 temperatura = {4} />
+    </div>
+  );
+}
+
+
